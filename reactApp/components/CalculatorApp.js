40a0a1399544d6c0ef5204fe4070ddccf9f29398
Samuel Lefcourt <samasherlefcourt@aol.com>
2017-12-29 21:42:49 -0800

    Modified about to start

--- reactApp/components/CalculatorApp.js
+++ reactApp/components/CalculatorApp.js
@@ -15,28 +15,7 @@ class TodoApp extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { todos: dummyData };
-  }
-
-  toggleTodo(index) {
-    // change DB
-    dummyData[index].completed = !dummyData[index].completed;
-    // setState out of DB
-    this.setState({ todos: dummyData });
-  }
-
-  addTodo(task) {
-    //add to DB
-    dummyData.push({ id: id++, task: task, completed: false });
-    // setState out of DB
-    this.setState({ todos: dummyData });
-  }
-
-  removeTodo(index) {
-    // remove from DB
-    dummyData.splice(index, 1);
-    // setState out of DB
-    this.setState({ todos: dummyData });
+    this.state = {  };
   }
 
   render() {
